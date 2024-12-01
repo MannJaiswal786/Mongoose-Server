56efa18aec44049bf7fc28d8fa8e845f7cedbf16
Mann Jaiswal <mannjaiswal5@gmail.com>
2024-11-30 17:50:23 -0700

    Added a sub document by creating comments schema to include comments for each campsite document created

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ connect.then(() => {
     console.log('Connected correctly to server');
 
     Campsite.create({
-        name: "React Lake Campground",
+        name: "React Lake Campgrounds",
         description: "test"
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite._id, {$set: {description: 'Updated Test Document'}}, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
